7a4708c8e23a98b33c7e8c197179e8c666518daf
AMC <39130921+k0y0t@users.noreply.github.com>
2022-08-01 22:06:03 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <a href="/sobr3">Vá para Sobre!</a>
             <Contador />
             <div>test</div>
         </div>        

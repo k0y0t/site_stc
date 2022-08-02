9a0cc7f63a0f6c50ab30a5b19392b25b4c9f420c
AMC <39130921+k0y0t@users.noreply.github.com>
2022-08-01 22:14:03 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,8 @@
-import { useState } from 'react';
-
 function Home() {
-    return (
-        <div>
+    return (<div>
             <h1>Hom3</h1>
             <a href="/sobre">Vá para Sobre!</a>
-            <Contador />
-            <div>test</div>
-        </div>        
-    )
-}
-
-function Contador() {
-    const [contador,setContador] = useState(1);
-    
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
+    </div>)
 }
 
 export default Home
